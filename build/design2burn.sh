e60163c7a9cf137e7bf915bcc4ca645dbac2c476
gkholman <gkenholman+github@gmail.com>
2024-09-01 09:46:16 -0400

    Rename stylesheet creating burn files

--- build/design2burn.sh
+++ build/design2burn.sh
@@ -1,7 +1,7 @@
 DP0=$( cd "$(dirname "$0")" ; pwd -P )
 pushd "$DP0/../burn" >/dev/null
-java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-revenge-crane.svg -xsl:../utilities/designSVG2burnPDF/designSVG2burnPDF.xsl -o:svg2svgpdfpng.sh path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-revenge-crane 
-#java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-revenge-crane.svg -xsl:../../designSVG2burnPDF/designSVG2burnPDF.xsl -o:svg2svgpdfpng.sh path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-revenge-crane 
+java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-revenge-crane.svg -xsl:../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl -o:svg2svgpdfpng.sh path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-revenge-crane 
+#java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-revenge-crane.svg -xsl:../../designSVG2burnFiles/designSVG2burnFiles.xsl -o:svg2svgpdfpng.sh path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-revenge-crane 
 errorReturn=$?
 if [ $errorReturn -ne 0 ]; then exit $errorReturn ; fi
 if [ -f svg2svgpdfpng.sh ]; then sh svg2svgpdfpng.sh ; fi
