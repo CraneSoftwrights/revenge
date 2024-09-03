12f4b69c467bccaa8a943d2f077f4ac1aec9642e
gkholman <gkenholman+github@gmail.com>
2024-09-03 13:50:17 -0400

    Change invocation of designSVG2burnFiles to check first that a sibling git repository is not available before using the utilities directory

--- build/design2burn.bat
+++ build/design2burn.bat
@@ -1,6 +1,12 @@
 rem @echo off
 pushd "%~dp0\..\burn"
-java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-revenge-crane.svg -xsl:../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl -o:svg2svgpdfpng.bat path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-revenge-crane
+
+set ss=stylesheet-not-found-designSVG2burnFiles.xsl
+if exist ../../designSVG2burnFiles/designSVG2burnFiles.xsl set ss=../../designSVG2burnFiles/designSVG2burnFiles.xsl
+if exist ../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl set ss=../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl
+
+java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-revenge-crane.svg -xsl:%ss% -o:svg2svgpdfpng.bat path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-revenge-crane
+
 set errorReturn=%ERRORLEVEL%
 if %errorReturn% neq 0 exit /b %errorReturn%
 if exist svg2svgpdfpng.bat call svg2svgpdfpng.bat
--- build/design2burn.sh
+++ build/design2burn.sh
@@ -1,7 +1,12 @@
 DP0=$( cd "$(dirname "$0")" ; pwd -P )
 pushd "$DP0/../burn" >/dev/null
-java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-revenge-crane.svg -xsl:../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl -o:svg2svgpdfpng.sh path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-revenge-crane 
-#java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-revenge-crane.svg -xsl:../../designSVG2burnFiles/designSVG2burnFiles.xsl -o:svg2svgpdfpng.sh path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-revenge-crane 
+
+ss=stylesheet-not-found-designSVG2burnFiles.xsl
+if [ -f ../../designSVG2burnFiles/designSVG2burnFiles.xsl ]; then ss=../../designSVG2burnFiles/designSVG2burnFiles.xsl ; fi
+if [ -f ../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl ]; then ss=../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl ; fi
+
+java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-revenge-crane.svg -xsl:$ss -o:svg2svgpdfpng.sh path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-revenge-crane 
+
 errorReturn=$?
 if [ $errorReturn -ne 0 ]; then exit $errorReturn ; fi
 if [ -f svg2svgpdfpng.sh ]; then sh svg2svgpdfpng.sh ; fi
