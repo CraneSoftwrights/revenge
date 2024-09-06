3dcf5b4785439d1663d13c2005c1eb093a501727
gkholman <gkenholman+github@gmail.com>
2024-09-06 16:42:27 -0400

    New arguments to design2burn

--- build/design2burn.bat
+++ build/design2burn.bat
@@ -5,7 +5,7 @@ set ss=stylesheet-not-found-designSVG2burnFiles.xsl
 if exist ../../designSVG2burnFiles/designSVG2burnFiles.xsl set ss=../../designSVG2burnFiles/designSVG2burnFiles.xsl
 if exist ../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl set ss=../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl
 
-java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-revenge-crane.svg -xsl:%ss% -o:svg2svgpdfpng.bat path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-revenge-crane
+java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-revenge-crane.svg -xsl:%ss% -o:svg2svgpdfpng.bat path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-revenge-crane minimum-stroke-width=.003in cut-colour=ff00ff
 
 set errorReturn=%ERRORLEVEL%
 if %errorReturn% neq 0 exit /b %errorReturn%
